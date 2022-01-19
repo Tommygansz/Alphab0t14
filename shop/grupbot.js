45ea071d50d3d31c87069dfacbb18a345f5b0e44
Toxxmy <90817660+Tommygansz@users.noreply.github.com>
2022-01-19 20:40:39 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -3,13 +3,13 @@ const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
 1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
+_https://chat.whatsapp.com/FF6H2hS4sv45zwHMRfslYB_
 2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
+_https://chat.whatsapp.com/I4ImyqOHX9VCbpYgJMDsQx_
 3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
+_https://chat.whatsapp.com/IGsTJXqdhxt35RFXlL3tzh_
 4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+_https://chat.whatsapp.com/KGGCuiqpcGS9p1FafDdhqS_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
